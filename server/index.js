438879c163e77c7867694aeca4acbd9e4683f7e1
shubham jayswal <shubhamjayswal10302@gmail.com>
2020-02-22 19:33:10 +0530

    changed file

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ csv()
 .fromFile(csvFilePath)
 .then((jsonObj)=>{
     const iplMatches = new Matches(jsonObj);
-    //console.log(iplMatches)
+    console.log(iplMatches)
     var matchesall = iplMatches.searchIplData();
     console.log((matchesall));
     var allData = JSON.stringify(matchesall);
@@ -29,19 +29,42 @@ csv()
     })
 
 
-    var iplBolwers = iplMatches.takeId()
-    //console.log(iplBolwers)
+var iplBolwers = iplMatches.takeId()
+ var bowlerseco = iplMatches.getMatchIdOfSpecificYear();
+ console.log(bowlerseco) 
     csv()
     .fromFile(csvFilePath2)
     .then((jsonObj)=>{
-        //console.log(jsonObj);
         const iplMatches1 = new Matches(jsonObj);
+
+
         var matchesall = iplMatches1.searchExtraRuns(iplBolwers)
         var allData2 = JSON.stringify(matchesall);
-    fs.writeFile('/home/shubham/Downloads/src/output/extraRuns.json', allData2,function(err,file){
+       fs.writeFile('/home/shubham/Downloads/src/output/extraRuns.json', allData2,function(err,file){
         if(err) throw err;
         console.log('saved');
-    })
-    })
+       }) 
+
+
+        var bowlerseconomic = iplMatches1.top10economicalBowlers(bowlerseco);
+        console.log(bowlerseconomic);
+   })
+
+
+
+var wonToss = iplMatches.wonTheTossAndWonTheMatch()
+console.log(wonToss)
+var allData4 = JSON.stringify(wonToss);
+fs.writeFile('/home/shubham/Downloads/src/output/ wonTosswonMatch.json', allData4,function(err,file){
+            if(err) throw err;
+            console.log('saved');
+ }) 
+
+
+var playrData = iplMatches.playerOfTheatch()
+console.log(playrData)
+
+
+
 })
 
--- server/ipl.js
+++ server/ipl.js
@@ -3,9 +3,12 @@ module.exports = class Matches {
       this.matchesData = jsonObj;
       this.obj1 = {}
       this.obj2 ={}
+      this.obj3 = {}
+      this.arrayofyear = []
+      this.obj5 = {}
       
     }
-    
+  
     searchIplData() {
     var arr = []
     for(let i in this.matchesData){
@@ -66,4 +69,111 @@ module.exports = class Matches {
      }
      return this.obj2
   }
-  };
+  
+
+
+  getMatchIdOfSpecificYear(){
+          //console.log(this.matchesData)
+          var arr1 = [],array = []
+          for(let i in this.matchesData){
+            if(this.matchesData[i].season === '2015'){
+              arr1.push(this.matchesData[i].id)
+            }
+          }
+          array.push(arr1[0]);
+          array.push(arr1[arr1.length - 1]);
+          return arr1
+  }
+
+  top10economicalBowlers(array1){
+    var bowlerRun = {}
+      for(let i in this.matchesData){   
+      if (array1.indexOf(this.matchesData[i].match_id != -1)){
+          if (bowlerRun.hasOwnProperty(this.matchesData[i].bowler)) {
+              bowlerRun[this.matchesData[i].bowler]['total_runs'] += parseInt(this.matchesData[i].batsman_runs)
+              +parseInt(this.matchesData[i].noball_runs)+parseInt(this.matchesData[i].wide_runs);
+              if (this.matchesData[i].noball_runs == "0" && this.matchesData[i].wide_runs == "0") {
+                  bowlerRun[this.matchesData[i].bowler]['total_balls'] +=1;
+              }
+           }else{
+            bowlerRun[this.matchesData[i].bowler] = {}
+            bowlerRun[this.matchesData[i].bowler]['total_runs'] = parseInt(this.matchesData[i].batsman_runs)
+              +parseInt(this.matchesData[i].noball_runs)+parseInt(this.matchesData[i].wide_runs);
+              if (this.matchesData[i].noball_runs == "0" && this.matchesData[i].wide_runs == "0") {
+                  bowlerRun[this.matchesData[i].bowler]['total_balls'] =1;
+              }else{
+                bowlerRun[this.matchesData[i].bowler]['total_balls'] =0;
+            }
+           }
+      }
+  }
+  let arr5 = [];
+      for (let k in bowlerRun) {
+        // (bowlerRun[k]['total_runs']/bowlerRun[k]['total_balls'])
+            let economicRate = (((bowlerRun[k]['total_runs'])/(bowlerRun[k]['total_balls']))*6);
+            let obj = {
+                 y: economicRate,
+                 bowler: k
+              }
+               arr5.push(obj);
+            }
+          //console.log(economicRate)
+    
+   arr5.sort(function (a, b) {
+      return a.y - b.y;
+ })
+  console.log(arr5)
+  //console.log(bowlerRun)
+  }
+
+
+wonTheTossAndWonTheMatch(){
+  console.log(this.matchesData)
+  var obj4 = {}
+  for(var i in this.matchesData){
+    if(obj4.hasOwnProperty(this.matchesData[i].toss_winner)){
+      if(this.matchesData[i].toss_winner === this.matchesData[i].winner){
+        obj4[this.matchesData[i].toss_winner] +=1
+        
+      }
+    }else{
+      if(this.matchesData[i].toss_winner === this.matchesData[i].winner){
+        obj4[this.matchesData[i].toss_winner] =1
+        
+      }
+    }
+  }
+  return obj4
+}
+
+
+playerOfTheatch(){
+  
+  //console.log(this.matchesData)
+  var year = '2008'
+         for(let s = 0; s <= 9; s++){ 
+           this.obj5 = {}
+          for(let j in this.matchesData){
+            if(this.matchesData[j].season == year){
+              this.arrayofyear.push(this.matchesData[j].id)
+            }
+          }
+          var t = 0
+          for(var i in this.matchesData){
+            if(this.matchesData[i].id === this.arrayofyear[t]){
+            if(this.obj5.hasOwnProperty([this.matchesData[i].player_of_match])){
+              this.obj5[this.matchesData[i].player_of_match] += 1
+            }else{
+              this.obj5[this.matchesData[i].player_of_match] = 1
+            }
+            t++
+          }
+        
+      }
+      //console.log(Object.keys(this.obj5).reduce((a, b) => this.obj5[a] > this.obj5[b] ? a : b))
+      console.log(this.obj5)
+      year++
+    }
+      
+    }
+};
